7ec5e0019ccf1e6a12017251369eab662954d28e
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-09-19 12:13:20 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+      {
+        name: 'NextAppName',
+        exec_mode: 'cluster',
+        instances: 'max', // Or a number of instances
+        script: 'node_modules/next/dist/bin/next',
+        args: 'start',
+        env_local: {
+          APP_ENV: 'local' // APP_ENV=local
+        },
+        env_development: {
+          APP_ENV: 'dev' // APP_ENV=dev
+        },
+        env_production: {
+          APP_ENV: 'prod',
+          PORT: 3001 //// APP_ENV=prod
+        }
+      }
+    ]
+  }
